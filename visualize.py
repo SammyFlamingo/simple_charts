9223f580fc72717c95785f86564d47122a09bc51
Alex Fivecoats <ultramegacoats@gmail.com>
2018-10-04 00:40:26 -0400

    increased speed removed some data looping

--- visualize.py
+++ visualize.py
@@ -8,12 +8,18 @@ unused_keys = ["", "bikeid", "end station latitude", "end station longitude", "e
                "start station latitude", "start station longitude", "start station id", "name_localizedValue0"]
 
 # enter the path to the csv
-csv_path = "file path"
+csv_path = "/home/alex/projects/simple_charts/citibike_sample.csv"
+current_year = datetime.today().year
+
+dicts_list = []
+trips_list = [[], [], []]
+age_list = []
+trip_averages = []
 
 
 # builds list of dicts
-def buildDictList(csv_location):
-    dict_reader = csv.DictReader(open(csv_location))
+def buildDictList():
+    dict_reader = csv.DictReader(open(csv_path))
     dicts = []
 
     # add each row to the list as a dict
@@ -24,66 +30,53 @@ def buildDictList(csv_location):
 
 
 # cleans the dicts
-def cleanData(list_of_dicts, list_keys_removed):
+# adds trip lengths to trips_list, separated by gender
+# calls calc_averages()
+# calls calc_ages()
+def wrangle_data():
+
     # pops the keys not desired in the dict
-    for dict in list_of_dicts:
-        for key in list_keys_removed:
+    for dict in buildDictList():
+        for key in unused_keys:
             dict.pop(key)
 
         # relabels genders to be easily identified
+        # separate the cleaned dicts into 3 lists of trip durations converted to  minutes
         if dict["gender"] == "0":
             dict["gender"] = "undisclosed"
+            trips_list[0].append((int(dict["tripduration"]) / 60))
         if dict["gender"] == "1":
             dict["gender"] = "male"
+            trips_list[1].append((int(dict["tripduration"]) / 60))
         if dict["gender"] == "2":
             dict["gender"] = "female"
+            trips_list[2].append((int(dict["tripduration"]) / 60))
 
-    return list_of_dicts
+        calc_ages(dict)
+    calc_averages()
+    age_list.sort()
+
+    return dicts_list, age_list
 
 
 # calculates ages from birthyear and current year
-def calcAges(dicts_list):
-    age_list = []
-    current_year = datetime.today().year
+def calc_ages(dict):
+    age = int(current_year) - int(dict["birth year"])
+    age_list.append(age)
 
-    for dict in dicts_list:
-        age = int(current_year) - int(dict["birth year"])
-        age_list.append(age)
-
-    age_list.sort()
-
-    return age_list
-
-
-# separate the cleaned list of dicts into 3 lists of trip durations
-
-def separateTrips(cleaned_dicts):
-    trips_list = [[], [], []]
-
-    # adding each gender's trips to their trip list and converting seconds to minutes
-    for dict in cleaned_dicts:
-        if dict["gender"] == "undisclosed":
-            trips_list[0].append((int(dict["tripduration"]) / 60))
-        if dict["gender"] == "male":
-            trips_list[1].append((int(dict["tripduration"]) / 60))
-        if dict["gender"] == "female":
-            trips_list[2].append((int(dict["tripduration"]) / 60))
-
-    return trips_list
+    return dict
 
 
 # calculate mean average of each trip list and append to list of averages
-def calcAvgs(trips_list):
-    trip_avgs = []
+def calc_averages():
+    for list_of_durations in trips_list:
+        trip_averages.append(np.average(list_of_durations))
 
-    for list in trips_list:
-        trip_avgs.append(np.average(list))
-
-    return trip_avgs
+    return trip_averages
 
 
 # returns a bar chart
-def barOfTrips(list_of_avgs):
+def trips_bar(list_of_avgs):
     genders = ["Undisclosed", "Male", "Female"]
 
     # set the number of ticks on the x-axis
@@ -106,31 +99,36 @@ def barOfTrips(list_of_avgs):
     bar_chart.set_title("Trip Averages by Gender")
     bar_chart.legend()
 
+    plt.show()
+
     return bar_chart
 
 
 # display pie chart of each gender as a percent of the whole sample
-def pieOfGenders(separated_trips):
+def gender_percents(separated_trips):
     labels = "Undisclosed", "Male", "Female"
-    tripcount = 0
+    trip_count = 0
     gender_percents = []
 
     # calculate the total number of trips
     for list in separated_trips:
-        tripcount = tripcount + len(list)
+        trip_count = trip_count + len(list)
     # calculate each gender's percent of the total
     for list in separated_trips:
-        gender_percents.append(len(list) / tripcount)
+        gender_percents.append(len(list) / trip_count)
 
     fig, pie_chart = plt.subplots()
     pie_chart.pie(gender_percents, explode=None, labels=labels, autopct="%1.1f%%")
     pie_chart.axis("equal")
     pie_chart.set_title("Genders as a Percent of the Sample")
 
+    plt.show()
+
     return pie_chart
 
 
-def histChart(list_of_ages):
+# display a histogram showing the distribution of ages in data
+def age_distribution(list_of_ages):
     x = list_of_ages
     num_bins = max(list_of_ages) - min(list_of_ages)
 
@@ -140,17 +138,36 @@ def histChart(list_of_ages):
 
     # sets attributes of the chart
     hist_chart.set_xlabel("Age of Rider")
-    hist_chart.set_xticks([20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70])
+    hist_chart.set_xticks(find_ticks())
     hist_chart.set_ylabel("Count of Riders")
     hist_chart.set_title("Number of Riders by Age")
+
+    plt.show()
 
     return hist_chart
 
 
-dicts_list = cleanData(buildDictList(csv_path), unused_keys)
-age_list = calcAges(dicts_list)
-separated_trips = separateTrips(dicts_list)
-avgs_list = calcAvgs(separated_trips)
-avg_trip_bar = barOfTrips(avgs_list)
-pie_chart = pieOfGenders(separated_trips)
-histogram_chart = histChart(age_list)
+# creates integer list at increments of 5
+def find_ticks():
+    ticks_list = []
+
+    if np.min(age_list) % 5 != 0:
+        tick = age_list[0] - 4
+        while tick % 5 != 0:
+            tick = tick + 1
+    else:
+        tick = np.min(age_list)
+
+    ticks_list.append(tick)
+
+    while tick <= np.max(age_list):
+        tick = tick + 5
+        ticks_list.append(tick)
+
+    return ticks_list
+
+
+wrangle_data()
+avg_trip_bar = trips_bar(trip_averages)
+gender_pie = gender_percents(trips_list)
+ages_histogram = age_distribution(age_list)
